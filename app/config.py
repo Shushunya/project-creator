e5395245eb917ba9c7d77add8451be15acfc62e7
Shushunya <mremarka314@gmail.com>
2025-07-25 22:57:25 +0100

    add help and version options

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 CURRENT_DIR = os.getcwd()
@@ -11,3 +12,12 @@ DEFAULT_PROJECT_NAME = os.getenv("DEFAULT_PROJECT_NAME", "Local new name")
 BASE_PATH = os.getenv("BASE_PATH", "D:\projects\project starter")
 TEMPLATES_PATH = os.getenv("TEMPLATES_PATH", "/templates")
 PROJECT_TEMPLATES_PATH = os.getenv("PROJECT_TEMPLATES_PATH", "/projects")
+
+USAGE_MSG = os.getenv("USAGE_MSG", "")
+EPILOG_MSG = os.getenv("EPILOG_MSG", "")
+DESCRIPTION_MSG = os.getenv("DESCRIPTION_MSG", "")
+
+HELP_PATH = os.getenv("HELP_PATH", "")
+HELP_NAME = os.getenv("HELP_NAME", "")
+HELP_TYPE = os.getenv("HELP_TYPE", "")
+HELP_VERSION = os.getenv("HELP_VERSION", "")
--- app/parser.py
+++ app/parser.py
@@ -1,29 +1,46 @@
 import argparse, pathlib
-from config import DEFAULT_PROJECT_TYPE, DEFAULT_PROJECT_NAME, CURRENT_DIR
+
+from config import (DEFAULT_PROJECT_TYPE, DEFAULT_PROJECT_NAME,
+                    USAGE_MSG, DESCRIPTION_MSG, EPILOG_MSG,
+                    HELP_PATH, HELP_NAME, HELP_TYPE, HELP_VERSION)
 from utils import get_available_types
 
-# TODO: add usage msg
-# TODO: add description
 
+# TODO: make toml and get version from there
+VERSION = "0.1.0"
 project_types = get_available_types()
 
 def get_args() -> dict:
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        usage=USAGE_MSG,
+        description=DESCRIPTION_MSG,
+        epilog = EPILOG_MSG
+    )
 
+    parser.add_argument(
+        "path",
+        type=pathlib.Path,
+        help=HELP_PATH,
+
+    )
     parser.add_argument(
         "-n", "--name",
         default=DEFAULT_PROJECT_NAME,
-        type=str)
+        type=str,
+        help=HELP_NAME
+    )
     parser.add_argument(
         "-t", "--type",
         choices=project_types,
         default=DEFAULT_PROJECT_TYPE,
-        help="Type of project to create (default: %(default)s)"
+        help=HELP_TYPE
     )
     parser.add_argument(
-        "path",
-        type=pathlib.Path,
-        default=CURRENT_DIR
+        "-v", "--version",
+        action="version",
+        version=f'%(prog)s {VERSION}',
+        help=HELP_VERSION
     )
+
     data = parser.parse_args()
     return vars(data)
